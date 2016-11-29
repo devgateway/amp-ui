82201b263d2a41717aa0811f592abe9cb54c0a71
Gabriel Inchauspe <ginchauspe@gmail.com>
2016-11-29 09:06:32 -0300

    AMPOFFLINE-57: Moved constants.

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -1,3 +1,9 @@
 export const BASE_URL = 'http://localhost:8080';
 
 export const COLLECTION_USERS = 'users';
+export const COLLECTION_WORKPACES = 'workspaces';
+
+export const DB_FILE_PREFIX = './database/';
+export const DB_FILE_EXTENSION = '.db';
+export const AKEY = 'key'; //TODO: Find a better way to store the key.
+export const DB_COMMON_DATASTORE_OPTIONS = {autoload: false, corruptAlertThreshold: 0};
