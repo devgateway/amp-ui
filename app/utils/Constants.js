1216423dda89353b79b14a697434c4778561c9dd
Gabriel Inchauspe <ginchauspe@gmail.com>
2016-12-01 12:05:23 -0300

    AMPOFFLINE-57: Small changes on DatabaseManager.

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -8,4 +8,4 @@ export const DB_FILE_PREFIX = './database/';
 export const DB_FILE_EXTENSION = '.db';
 export const AKEY = 'key'; //TODO: Find a better way to store the key.
 export const DB_COMMON_DATASTORE_OPTIONS = {autoload: false, corruptAlertThreshold: 0};
-export const DB_AUTOCOMPACT_INTERVAL_SECONDS = 30;
+export const DB_AUTOCOMPACT_INTERVAL_MILISECONDS = 60000;
