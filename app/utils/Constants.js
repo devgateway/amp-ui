bb5f4e62c53133230acb423597199311056af791
Gabriel Inchauspe <ginchauspe@gmail.com>
2016-12-13 21:44:58 -0300

    AMPOFFLINE-74: Implemented unit-test for reducer.

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -9,3 +9,6 @@ export const DB_FILE_EXTENSION = '.db';
 export const AKEY = 'key'; //TODO: Find a better way to store the key.
 export const DB_COMMON_DATASTORE_OPTIONS = {autoload: false, corruptAlertThreshold: 0};
 export const DB_AUTOCOMPACT_INTERVAL_MILISECONDS = 60000;
+
+export const LANGUAGE_ENGLISH = 'en';
+export const LANGUAGE_SPANISH = 'es';
