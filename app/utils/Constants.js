1da4cc07e0fea7d9b0e9b2126e7164144e22deda
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-01-12 18:17:53 -0300

    AMPOFFLINE-158 Fixed some eslint errors when from 85 warnings1284 errors to 56 warnings 981 errors

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -15,13 +15,12 @@ export const COLLECTION_CLIENT_SETTINGS = 'client-settings';
 
 export const DB_FILE_PREFIX = './database/';
 export const DB_FILE_EXTENSION = '.db';
-export const AKEY = 'key'; //TODO: Find a better way to store the key.
-export const DB_COMMON_DATASTORE_OPTIONS = {autoload: false, corruptAlertThreshold: 0};
+//TODO: Find a better way to store the key.
+export const AKEY = 'key';
+export const DB_COMMON_DATASTORE_OPTIONS = { autoload: false, corruptAlertThreshold: 0 };
 export const DB_AUTOCOMPACT_INTERVAL_MILISECONDS = 60000;
 
 export const LANGUAGE_ENGLISH = 'en';
 export const LANGUAGE_SPANISH = 'es';
 
 export const HASH_ITERATIONS = 100;
-
-
