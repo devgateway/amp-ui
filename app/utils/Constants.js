8c5d0266d38c0813b0efaa9b1ea071775e8e412d
ginchauspe <ginchauspe@gmail.com>
2017-01-18 18:24:12 +0200

    AMPOFFLINE-105: Added function to generate SHA1/SHA256 digest. (#21)
    * AMPOFFLINE-105: Added function to generate SHA1/SHA256 digest.
    
    * AMPOFFLINE-105: Added sha(password) for login.
    
    * AMPOFFLINE-105: Moved sha(password) to online login only.

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -15,7 +15,7 @@ export const COLLECTION_CLIENT_SETTINGS = 'client-settings';
 
 export const DB_FILE_PREFIX = './database/';
 export const DB_FILE_EXTENSION = '.db';
-//TODO: Find a better way to store the key.
+// TODO: Find a better way to store the key.
 export const AKEY = 'key';
 export const DB_COMMON_DATASTORE_OPTIONS = { autoload: false, corruptAlertThreshold: 0 };
 export const DB_AUTOCOMPACT_INTERVAL_MILISECONDS = 60000;
@@ -24,3 +24,5 @@ export const LANGUAGE_ENGLISH = 'en';
 export const LANGUAGE_SPANISH = 'es';
 
 export const HASH_ITERATIONS = 100;
+export const DIGEST_ALGORITHM_SHA1 = 'SHA-1';
+export const DIGEST_ALGORITHM_SHA256 = 'SHA-256';
