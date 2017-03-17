f8fc1452d9c0db01cfb2197554b75ae9d09e8d82
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-03-17 18:53:37 +0200

    AMPOFFLINE-121 activities sync up

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -3,7 +3,7 @@ export const SERVER_URL = '__SERVER_URL__';
 export const BASE_REST_URL= '/rest';
 export const PROTOCOL = '__SERVER_PROTOCOL__';
 export const BASE_PORT = '__SERVER_PORT__';
-export const CONNECTION_TIMEOUT = 5000;
+export const CONNECTION_TIMEOUT = 15000;
 export const MAX_RETRY_ATEMPTS = 5;
 export const ERRORS_TO_RETRY = ['ESOCKETTIMEDOUT', 'ETIMEDOUT'];
 export const CONNECTIVITY_CHECK_INTERVAL ="3000";//"300000";//5 minutes
@@ -57,3 +57,6 @@ export const SYNCUP_TYPE_FIELDS = 'fields';
 export const SYNCUP_STATUS_SUCCESS = 'SUCCESS';
 export const SYNCUP_STATUS_FAIL = 'FAIL';
 export const SYNCUP_DATETIME_FIELD = 'timestamp';
+
+// TODO this is a temporary limitation of the initial set of activities to export, AMPOFFLINE-273 or AMPOFFLINE-274
+export const FIRST_ACTIVITIES_EXPORT_LIMIT = 50;
