93b876f5ba8148da91de579baa4d257c981f2338
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-03-28 17:51:06 +0300

    AMPOFFLINE-246 activity initial infrastructure,initial activity preview

---
+++ app/utils/constants/FieldPathConstants.js
@@ -0,0 +1,7 @@
+/**
+ * This is a set of field paths that are used for frequent needs
+ * @author Nadejda Mandrescu
+ */
+
+export const ADJUSTMENT_TYPE_PATH = 'funding~funding_details~adjustment_type';
+export const TRANSACTION_TYPE_PATH = 'funding~funding_details~transaction_type';
