ad83a1ad65a758aa347fd434fe2a8ec741d572ed
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-04-17 23:23:05 +0200

    AMPOFFLINE-309 add Location section

--- app/utils/constants/FieldPathConstants.js
+++ app/utils/constants/FieldPathConstants.js
@@ -9,6 +9,7 @@ export const FUNDING_DETAILS_PATH = 'fundings~funding_details';
 export const ADJUSTMENT_TYPE_PATH = 'fundings~funding_details~adjustment_type';
 export const TRANSACTION_TYPE_PATH = 'fundings~funding_details~transaction_type';
 export const ACTIVITY_INTERNAL_IDS_INTERNAL_ID_PATH = 'activity_internal_ids~internal_id';
+export const LOCATION_PATH = 'locations~location';
 
 export const RICH_TEXT_FIELDS = new Set([AC.STATUS_REASON, AC.OBJECTIVE, AC.DESCRIPTION, AC.PROJECT_COMMENTS,
   AC.LESSONS_LEARNED, AC.PROJECT_IMPACT, AC.ACTIVITY_SUMMARY, AC.CONDITIONALITIES, AC.PROJECT_MANAGEMENT,
