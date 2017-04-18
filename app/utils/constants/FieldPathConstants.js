e8c6edebe9c5293814d7dc79cd4286210ab21208
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-04-18 23:07:05 +0200

    AMPOFFLINE-309 generic percentage list, using it for NPO and Locations

--- app/utils/constants/FieldPathConstants.js
+++ app/utils/constants/FieldPathConstants.js
@@ -10,7 +10,13 @@ export const ADJUSTMENT_TYPE_PATH = 'fundings~funding_details~adjustment_type';
 export const TRANSACTION_TYPE_PATH = 'fundings~funding_details~transaction_type';
 export const ACTIVITY_INTERNAL_IDS_INTERNAL_ID_PATH = 'activity_internal_ids~internal_id';
 export const LOCATION_PATH = 'locations~location';
+export const NATIONAL_PLAN_OBJECTIVE_PATH = 'national_plan_objective~program';
+export const PRIMARY_PROGRAM_PATH = 'primary_programs~program';
+export const SECONDARY_PROGRAM_PATH = 'secondary_programs~program';
 
 export const RICH_TEXT_FIELDS = new Set([AC.STATUS_REASON, AC.OBJECTIVE, AC.DESCRIPTION, AC.PROJECT_COMMENTS,
   AC.LESSONS_LEARNED, AC.PROJECT_IMPACT, AC.ACTIVITY_SUMMARY, AC.CONDITIONALITIES, AC.PROJECT_MANAGEMENT,
 ]);
+
+export const PATHS_WITH_FULL_VALUE = new Set([LOCATION_PATH, NATIONAL_PLAN_OBJECTIVE_PATH, PRIMARY_PROGRAM_PATH,
+  SECONDARY_PROGRAM_PATH]);
