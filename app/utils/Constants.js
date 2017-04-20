e3f4dbd87cbcea57fa73f53ff9f3735830e96ba5
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-04-20 14:27:17 -0300

    AMPOFFLINE-138 AMP Offline similar to AMP design for the header, menu and footer

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -11,6 +11,7 @@ export const CONNECTIVITY_CHECK_INTERVAL = '60000'; // 1 minutes
 
 export const WORKSPACE_URL = '/workspace';
 export const LOGIN_URL = '/';
+export const DESKTOP_URL = '/desktop';
 export const ACTIVITY_PREVIEW_URL = '/activity/preview';
 export const ACTIVITY_EDIT_URL = '/activity/edit';
 export const SYNCUP_URL = '/syncUp';
@@ -51,6 +52,7 @@ export const SYNCUP_BEST_BEFORE_DAYS = 7;
 export const SYNCUP_NO_DATE = '1900-01-01T00:00:00.000+0000';
 export const SYNCUP_TYPE_TRANSLATIONS = 'translations';
 export const SYNCUP_TYPE_USERS = 'users';
+export const SYNCUP_TYPE_ASSETS = 'assets';
 export const SYNCUP_TYPE_WORKSPACES = 'workspaces';
 export const SYNCUP_TYPE_GS = 'global-settings';
 export const SYNCUP_TYPE_WORKSPACE_MEMBERS = 'workspace-members';
@@ -66,6 +68,10 @@ export const ACTIVITY_STATUS_UNVALIDATED = 'ACTIVITY_STATUS_UNVALIDATED';
 export const ACTIVITY_STATUS_VALIDATED = 'ACTIVITY_STATUS_VALIDATED';
 
 export const VERSION = '0.0.1';
+export const BASE_64_PNG_PREFIX = 'data:image/png;base64,';
+// Holds the BASE64 representation of a a transparent image 1x1 in case we don't yet have the country flag
+export const
+  TRANSPARENT_FLAG = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=';
 // TODO this is a temporary limitation of the initial set of activities to export, AMPOFFLINE-273 or AMPOFFLINE-274
 export const FIRST_ACTIVITIES_EXPORT_LIMIT = 50;
 export const FIRST_ACTIVITIES_PULL_FROM_AMP_LIMIT = 500;
