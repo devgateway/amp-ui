2494c9e2da69504b014e5681415de44a7a0e297c
anyaelise <anya.elise.marshall@gmail.com>
2017-05-10 16:10:30 -0500

    Add header section

--- ActivityPreview.jsx
+++ ActivityPreview.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import styles from './ActivityPreview.css';
 import translate from '../../../utils/translate';
 import * as AC from '../../../utils/constants/ActivityConstants';
@@ -8,6 +8,11 @@ import MainGroup from './MainGroup';
 import ActivityFieldsManager from '../../../modules/activity/ActivityFieldsManager';
 import ActivityFundingTotals from '../../../modules/activity/ActivityFundingTotals';
 import LoggerManager from '../../../modules/util/LoggerManager';
+import edit from '../../../../assets/AMP_ProjectIcon1.svg';
+import version from '../../../../assets/AMP_ProjectIcon3.svg';
+import print from '../../../../assets/AMP_ProjectIcon2.svg';
+import pdf from '../../../../assets/pdf_icon.svg';
+import word from '../../../../assets/word_icon.svg';
 
 /**
  * Activity Preview main container
@@ -66,12 +71,64 @@ export default class ActivityPreview extends Component {
     const style = {
       verticalAlign: 'top'
     };
+    const categoryArray = ['Identification', 'Agency Internal IDs', 'Planning',
+      'Location', 'National Plan', 'Program', 'Sectors'];
+    const categories = categoryArray.map((category) =>
+      <li>{category}</li>
+    );
+    const editTooltip = (<Tooltip id="editTooltip">Edit</Tooltip>);
+    const versionTooltip = (<Tooltip id="versionTooltip">Version History</Tooltip>);
+    const pdfTooltip = (<Tooltip id="pdfTooltip">Save as PDF</Tooltip>);
+    const wordTooltip = (<Tooltip id="wordTooltip">Save as Word Doc</Tooltip>);
+    const printTooltip = (<Tooltip id="printTooltip">Print</Tooltip>);
 
-    // TODO a proper styling will come once we get UI Desing from Llanco
+    console.log(this.props);
     return (
       <div className={styles.preview_container} >
         <div className={styles.preview_header} >
           <span className={styles.preview_title} >{activity[AC.PROJECT_TITLE]}</span>
+          <span className={styles.preview_icons} >
+            <ul>
+              <li>
+                <OverlayTrigger placement="top" overlay={editTooltip}>
+                  <object type={'image/svg+xml'} data={edit} className={styles.edit}> Edit </object>
+                </OverlayTrigger>
+              </li>
+              <li>
+                <OverlayTrigger placement="top" overlay={versionTooltip}>
+                  <img src={version} alt="Version History"/>
+                </OverlayTrigger>
+              </li>
+              <li>
+                <OverlayTrigger placement="top" overlay={pdfTooltip}>
+                  <object type={'image/svg+xml'} data={pdf}> Save as PDF </object>
+                </OverlayTrigger>
+              </li>
+              <li>
+                <OverlayTrigger placement="top" overlay={wordTooltip}>
+                  <object type={'image/svg+xml'} data={word}> Save as Word Doc </object>
+                </OverlayTrigger>
+              </li>
+              <li>
+                <OverlayTrigger placement="top" overlay={printTooltip}>
+                  <img className={styles.print} src={print} alt="Print"/>
+                </OverlayTrigger>
+              </li>
+            </ul>
+          </span>
+        </div>
+        <div className={styles.preview_status} >
+          <span className={styles.preview_status_title} > AMP ID: </span>
+          <span className={styles.preview_status_detail} >{activity.amp_id} </span>
+          <span className={styles.preview_status_title} > Status: </span>
+          <span className={styles.preview_status_detail} > Ongoing </span>
+          <span className={styles.preview_status_title} > On/Off Budget: </span>
+          <span className={styles.preview_status_detail} > On Treasury </span>
+        </div>
+        <div className={styles.preview_categories} >
+          <ul>
+            {categories}
+          </ul>
         </div>
         <div>
           <Grid fluid>
