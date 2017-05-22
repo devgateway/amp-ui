f5d9b880e94f02bc01a7776812491bfd2af2db37
Alexei Savca <savca.alexei@gmail.com>
2017-05-22 10:04:38 +0300

    AMPOFFLINE-404 Showing missing fields in APIndentification

--- sections/APIdentification.jsx
+++ sections/APIdentification.jsx
@@ -20,22 +20,7 @@ class APIdentification extends Component {
 
   render() {
     const {buildSimpleField} = this.props;
-    const fieldPaths = [
-      AC.PROJECT_TITLE,
-      AC.AMP_ID,
-      AC.ACTIVITY_STATUS,
-      AC.STATUS_REASON,
-      AC.TYPE_OF_IMPLEMENTATION,
-      AC.MODALITIES,
-      AC.OBJECTIVE,
-      AC.DESCRIPTION,
-      AC.PROJECT_COMMENTS,
-      AC.LESSONS_LEARNED,
-      AC.PROJECT_IMPACT,
-      AC.ACTIVITY_SUMMARY,
-      AC.CONDITIONALITIES,
-      AC.PROJECT_MANAGEMENT,
-      AC.A_C_CHAPTER];
+    const fieldPaths = [AC.PROJECT_TITLE, AC.AMP_ID, AC.ACTIVITY_STATUS, AC.STATUS_REASON, AC.TYPE_OF_IMPLEMENTATION, AC.MODALITIES, AC.OBJECTIVE, AC.DESCRIPTION, AC.PROJECT_COMMENTS, AC.LESSONS_LEARNED, AC.PROJECT_IMPACT, AC.ACTIVITY_SUMMARY, AC.ACTIVITY_BUDGET, AC.CONDITIONALITIES, AC.PROJECT_MANAGEMENT, AC.A_C_CHAPTER];
     return (
       <div>
         {fieldPaths.map(fieldPath => buildSimpleField(fieldPath, true))}
