3285cc7721da9d5297993737884bbf120aaad7e8
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-06-13 18:43:30 -0300

    AMPOFFLINE-434: Adding Proposed Project Cost.

--- app/utils/constants/ActivityConstants.js
+++ app/utils/constants/ActivityConstants.js
@@ -120,6 +120,7 @@ export const COMPONENT_TYPE = 'component_type';
 export const COMPONENT_TITLE = 'component_title';
 export const COMPONENT_FUNDING = 'funding';
 export const COMPONENT_DESCRIPTION = 'description';
+export const FUNDING_AMOUNT_ID = 'amp_funding_amount_id';
 
 export function toFieldNames(listOfNames) {
   return listOfNames.map(name => toFieldName(name));
