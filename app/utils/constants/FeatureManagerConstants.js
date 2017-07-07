1e0570cef70e838f271afd6b2c306df0111f94d5
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-07-07 23:33:28 +0300

    AMPOFFLINE-441 FM storage, storage unit tests, sync up

---
+++ app/utils/constants/FeatureManagerConstants.js
@@ -0,0 +1,9 @@
+/**
+ * @author Nadejda Mandrescu
+ */
+
+export const PUBLIC_VIEW_CHANGE_PASSWORD = '/PUBLIC VIEW/Login - User Management/Change Password';
+export const PUBLIC_VIEW_TROUBLE_SIGN_IN = '/PUBLIC VIEW/Login - User Management/Trouble signing in';
+export const ACTIVITY_DELIVERY_RATE = '/PROJECT MANAGEMENT/Funding/Funding Information/Delivery rate';
+export const ACTIVITY_DURATION_OF_PROJECT = '/PROJECT MANAGEMENT/Project ID and Planning/Planning/Duration of Project';
+export const ACTIVITY_TEAM_LEADER = '/PROJECT MANAGEMENT/Project ID and Planning/Identification/Data Team Leader';
