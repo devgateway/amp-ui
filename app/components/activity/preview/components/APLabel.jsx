0afbfd56d53fc913683b63b30904bd13e3219496
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-07-10 18:49:07 -0300

    AMPOFFLINE-425: Add tooltip to APLabel and fix some translations.

--- app/components/activity/preview/components/APLabel.jsx
+++ app/components/activity/preview/components/APLabel.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import translate from '../../../../utils/translate';
 import LoggerManager from '../../../../modules/util/LoggerManager';
 
@@ -9,7 +10,8 @@ export default class APLabel extends Component {
     dontTranslate: PropTypes.bool,
     labelClass: PropTypes.string,
     /* decorator: PropTypes.func, // TODO: To be implemented. */
-    separator: PropTypes.bool
+    separator: PropTypes.bool,
+    tooltip: PropTypes.string
   };
 
   constructor(props) {
@@ -17,10 +19,23 @@ export default class APLabel extends Component {
     LoggerManager.log('constructor');
   }
 
-  render() {
+  getContent() {
     const label = (this.props.dontTranslate === true ? this.props.label : translate(this.props.label));
     const labelClass = (this.props.labelClass ? this.props.labelClass : '');
-    const component = (<div className={labelClass}><span>{label}</span>{this.props.separator ? <hr /> : ''}</div>);
-    return component;
+    return (<div className={labelClass} ><span>{label}</span>{this.props.separator ? <hr /> : ''}</div>);
+  }
+
+  tooltip() {
+    return <Tooltip id="tooltip" >{translate(this.props.tooltip)}</Tooltip>;
+  }
+
+  render() {
+    if (this.props.tooltip) {
+      return (<OverlayTrigger placement="right" overlay={this.tooltip()} >
+        {this.getContent()}
+      </OverlayTrigger>);
+    } else {
+      return this.getContent();
+    }
   }
 }
