5066458244a525e8476ff838e19a242ab24d74f7
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-07-10 15:29:33 -0300

    AMPOFFLINE-425: Check if Commitments/Disbursements/Expenditures is enabled.

--- sections/FundingSummary.jsx
+++ sections/FundingSummary.jsx
@@ -3,6 +3,7 @@ import Section from './Section';
 import APField from '../components/APField';
 import * as VC from '../../../../utils/constants/ValueConstants';
 import * as PC from '../../../../utils/constants/FieldPathConstants';
+import * as AC from '../../../../utils/constants/ActivityConstants';
 import ActivityFieldsManager from '../../../../modules/activity/ActivityFieldsManager';
 import ActivityFundingTotals from '../../../../modules/activity/ActivityFundingTotals';
 import translate from '../../../../utils/translate';
@@ -36,18 +37,21 @@ class FundingSummary extends Component {
     const measuresOrder = [VC.ACTUAL_COMMITMENTS, VC.PLANNED_COMMITMENTS, VC.ACTUAL_DISBURSEMENTS,
       VC.PLANNED_DISBURSEMENTS, VC.ACTUAL_EXPENDITURES, VC.UNALLOCATED_DISBURSEMENTS, VC.PLANNED_EXPENDITURES];
     const measuresTotals = {};
-    // TODO update how we detect that a transaction type is enabled once AMP-25787 is done
     // Commitments, Disbursements, Expenditures
     VC.TRANSACTION_TYPES.forEach(trnType => {
-      // checking if this transaction type is provided as an option, through a trick by detecting the translation
-      const trnTypeTrn = this.props.activityFieldsManager.getValueTranslation(PC.TRANSACTION_TYPE_PATH, trnType);
-      VC.ADJUSTMENT_TYPES.forEach(adjType => {
-        const adjTypeTrn = this.props.activityFieldsManager.getValueTranslation(PC.ADJUSTMENT_TYPE_PATH, adjType);
-        if (adjTypeTrn && trnTypeTrn) {
-          const value = this.props.activityFundingTotals.getTotals(adjTypeTrn, trnTypeTrn, {});
-          measuresTotals[`${adjType} ${trnType}`] = value;
-        }
-      });
+      const pv = this.props.activityFieldsManager.possibleValuesMap[PC.TRANSACTION_TYPE_PATH];
+      const enabledTrnList = Object.keys(pv).map((i) => (pv[i].value));
+      if (enabledTrnList.filter((t) => (t.value === trnType))) {
+        // checking if this transaction type is provided as an option, through a trick by detecting the translation
+        const trnTypeTrn = this.props.activityFieldsManager.getValueTranslation(PC.TRANSACTION_TYPE_PATH, trnType);
+        VC.ADJUSTMENT_TYPES.forEach(adjType => {
+          const adjTypeTrn = this.props.activityFieldsManager.getValueTranslation(PC.ADJUSTMENT_TYPE_PATH, adjType);
+          if (adjTypeTrn && trnTypeTrn) {
+            const value = this.props.activityFundingTotals.getTotals(adjTypeTrn, trnTypeTrn, {});
+            measuresTotals[`${adjType} ${trnType}`] = value;
+          }
+        });
+      }
     });
     // Other measures
     const adjTypeActualTrn = this.props.activityFieldsManager.getValueTranslation(PC.ADJUSTMENT_TYPE_PATH, VC.ACTUAL);
