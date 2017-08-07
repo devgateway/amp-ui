b95ea5a1412cac1944c9f7700c5c560905e10f1e
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-08-07 12:38:45 +0300

    AMPOFFLINE-487 increase connection timeout; use abort interval formula

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -3,8 +3,7 @@ export const SERVER_URL = '__SERVER_URL__';
 export const BASE_REST_URL= '/rest';
 export const PROTOCOL = '__SERVER_PROTOCOL__';
 export const BASE_PORT = '__SERVER_PORT__';
-// TODO: we'll update it to a higher value during AMPOFFLINE-487
-export const CONNECTION_TIMEOUT = 5000;
+export const CONNECTION_TIMEOUT = 25000;
 export const MAX_RETRY_ATEMPTS = 5;
 export const ERRORS_TO_RETRY = ['ESOCKETTIMEDOUT', 'ETIMEDOUT', 'ECONNRESET', 'EAI_AGAIN'];
 export const ERROR_NO_AMP_SERVER = 'ECONNREFUSED';
