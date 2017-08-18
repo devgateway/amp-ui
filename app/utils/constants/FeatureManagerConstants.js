4e70c8b119a261fbbfb9c8636ec6a1282c0f4a49
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-08-18 11:02:09 +0300

    AMPOFFLINE-655 add same as x date to the Planning section

--- app/utils/constants/FeatureManagerConstants.js
+++ app/utils/constants/FeatureManagerConstants.js
@@ -7,5 +7,9 @@ export const PUBLIC_VIEW_TROUBLE_SIGN_IN = '/PUBLIC VIEW/Login - User Management
 export const ACTIVITY_DELIVERY_RATE = '/PROJECT MANAGEMENT/Funding/Funding Information/Delivery rate';
 export const ACTIVITY_DURATION_OF_PROJECT = '/PROJECT MANAGEMENT/Project ID and Planning/Planning/Duration of Project';
 export const ACTIVITY_TEAM_LEADER = '/PROJECT MANAGEMENT/Project ID and Planning/Identification/Data Team Leader';
+export const ACTIVITY_SAME_AS_PROPOSED_START_DATE =
+  '/Activity Form/Planning/Actual Start Date/Same as Proposed Start Date';
+export const ACTIVITY_SAME_AS_PROPOSED_APPROVAL_DATE =
+  '/Activity Form/Planning/Actual Approval Date/Same as Proposed Approval Date';
 export const ACTIVITY_LAST_UPDATED_BY = '/Activity Form/Identification/Activity Last Updated by';
 export const ACTIVITY_LAST_UPDATED_ON = '/Activity Form/Identification/Activity Updated On';
