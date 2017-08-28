2466aab8d4a258b0fbd57bef97294a5b6848da23
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-08-28 10:24:17 +0300

    AMPOFFLINE-705 reduce unit tests speed - make some classes/objects and tests independent from store - exclude temporary a few store dependent unit tests to run on Jenkins

--- app/modules/util/FeatureManager.js
+++ app/modules/util/FeatureManager.js
@@ -1,36 +1,47 @@
-import store from '../../index';
 import LoggerManager from './LoggerManager';
 
 /**
  * Feature Manager
  * @author Nadejda Mandrescu
  */
-const FeatureManager = {
+export default class FeatureManager {
+  static _currentFM = new FeatureManager();
+
+  constructor(fmTree) {
+    this._fmTree = fmTree;
+  }
+
+  set fmTree(fmTree) {
+    this._fmTree = fmTree;
+  }
+
+  static setFMTree(fmTree) {
+    FeatureManager._currentFM.fmTree = fmTree;
+  }
+
   /**
    * Checks if the given FM path is fully enabled or only last segment is enabled
    * @param fmPath the FM path, e.g. '/PROJECT MANAGEMENT/Funding/Funding Information/Delivery rate'
    * @param onlyLastSegment specifies if to check if only the last segment is enabled (the AMP behavior for some cases)
    * @return {boolean}
    */
+  static isFMSettingEnabled(fmPath, onlyLastSegment) {
+    return FeatureManager._currentFM.isFMSettingEnabled(fmPath, onlyLastSegment);
+  }
+
   isFMSettingEnabled(fmPath, onlyLastSegment) {
-    LoggerManager.log('isFMSettingEnabled');
-    const fmTree = store.getState().startUpReducer.fmTree;
-    if (fmTree) {
-      return this._isFMSettingEnabled(fmPath, onlyLastSegment, fmTree);
+    LoggerManager.debug('isFMSettingEnabled');
+    // store.getState().startUpReducer.fmTree;
+    if (this._fmTree) {
+      let lastFMSubTree = this._fmTree;
+      // ignore first "/" to exclude empty string from the split
+      const segments = fmPath.substring(1).split('/');
+      const foundLastFMSubTree = segments.every(segment => {
+        lastFMSubTree = lastFMSubTree[segment];
+        return lastFMSubTree !== undefined && (lastFMSubTree.__enabled || onlyLastSegment);
+      });
+      return foundLastFMSubTree && lastFMSubTree.__enabled;
     }
     return false;
-  },
-
-  _isFMSettingEnabled(fmPath, onlyLastSegment, fmTree) {
-    let lastFMSubTree = fmTree;
-    // ignore first "/" to exlcude empty string from the split
-    const segments = fmPath.substring(1).split('/');
-    const foundLastFMSubTree = segments.every(segment => {
-      lastFMSubTree = lastFMSubTree[segment];
-      return lastFMSubTree !== undefined && (lastFMSubTree.__enabled || onlyLastSegment);
-    });
-    return foundLastFMSubTree && lastFMSubTree.__enabled;
   }
-};
-
-export default FeatureManager;
+}
