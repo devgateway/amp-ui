9db31e2fdaabd8e559c9275ab81d8508cd52bfaa
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-09-07 12:24:11 +0300

    AMPOFFLINE-655 fixed FM path for "same as X date" check

--- app/utils/constants/FeatureManagerConstants.js
+++ app/utils/constants/FeatureManagerConstants.js
@@ -8,8 +8,8 @@ export const ACTIVITY_DELIVERY_RATE = '/PROJECT MANAGEMENT/Funding/Funding Infor
 export const ACTIVITY_DURATION_OF_PROJECT = '/PROJECT MANAGEMENT/Project ID and Planning/Planning/Duration of Project';
 export const ACTIVITY_TEAM_LEADER = '/PROJECT MANAGEMENT/Project ID and Planning/Identification/Data Team Leader';
 export const ACTIVITY_SAME_AS_PROPOSED_START_DATE =
-  '/Activity Form/Planning/Actual Start Date/Same as Proposed Start Date';
+  '/Activity Form/Planning/Actual Start Date/Same As Proposed Start Date';
 export const ACTIVITY_SAME_AS_PROPOSED_APPROVAL_DATE =
-  '/Activity Form/Planning/Actual Approval Date/Same as Proposed Approval Date';
+  '/Activity Form/Planning/Actual Approval Date/Same As Proposed Approval Date';
 export const ACTIVITY_LAST_UPDATED_BY = '/Activity Form/Identification/Activity Last Updated by';
 export const ACTIVITY_LAST_UPDATED_ON = '/Activity Form/Identification/Activity Updated On';
