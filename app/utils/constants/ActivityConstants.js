e11ff672f150b845cb79fd0d5dd63d68450617f9
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-09-27 15:44:03 -0300

    AMPOFFLINE-444: Replicate logic from AMP to show validation icon.

--- app/utils/constants/ActivityConstants.js
+++ app/utils/constants/ActivityConstants.js
@@ -187,6 +187,8 @@ export const WS_VALIDATION_ALL_EDIT = 'allEdits';
 export const WS_VALIDATION_NEW_ONLY = 'newOnly';
 export const WS_VALIDATION_FIELD = 'validation';
 export const CROSS_TEAM_VALIDATION = 'is-cross-team-validation';
+export const WS_ACCESS_TYPE_MANAGEMENT = 'Management';
+export const WS_ACCESS_TYPE_TEAM = 'Team';
 
 /** Roles (from WorkspaceMemberRoleConstants.java) **/
 export const ROLE_TEAM_MEMBER_WS_MANAGER = 1;
@@ -196,6 +198,7 @@ export const ROLE_TEAM_MEMBER_WS_APPROVER = 3;
 /** Other fields **/
 export const WORKSPACE_ID = 'workspace-id';
 export const ROLE_ID = 'role-id';
+export const ACCESS_TYPE = 'access-type';
 
 export function toFieldNames(listOfNames) {
   return listOfNames.map(name => toFieldName(name));
