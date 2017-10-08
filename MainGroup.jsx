e2f267e624f73b44572b1ab54026dc06e907827a
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-10-08 12:35:55 -0300

    AMPOFFLINE-426: Add some checks.

--- MainGroup.jsx
+++ MainGroup.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './ActivityPreview.css';
 import * as AC from '../../../utils/constants/ActivityConstants';
+import * as FMC from '../../../utils/constants/FeatureManagerConstants';
 import APIdentification from './sections/APIdentification';
 import { APInternalIds } from './sections/APInternalIds';
 import APPlanning from './sections/APPlanning';
@@ -28,10 +29,11 @@ export default class MainGroup extends Component {
   render() {
     // TODO (iteration 2+) hide sections that are not directly connected to a single field (e.g. planning, program)
     return (<div className={styles.main_group_container}>
-      <APIdentification />
+      <APIdentification fmPath={FMC.ACTIVITY_IDENTIFICATION} />
       <APInternalIds
         sectionPath={AC.ACTIVITY_INTERNAL_IDS} />
       <APPlanning
+        fmPath={FMC.ACTIVITY_PROJECT_ID_AND_PLANNING}
         inline={false}
         fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} />
       <APLocation
@@ -46,9 +48,9 @@ export default class MainGroup extends Component {
       <APSector
         fieldNameClass={styles.sector_title} fieldValueClass={''}
         percentTitleClass={styles.percent_field_name} percentValueClass={styles.percent_field_value} />
-      <APFundingSources fieldValueClass={styles.box_field_value} />
+      <APFundingSources sourcePath={AC.TOTAL_NUMBER_OF_FUNDING_SOURCES} fieldValueClass={styles.box_field_value} />
       <APFundingSection
-        fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} />
+        fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} sectionPath={AC.FUNDINGS} />
       <APRelatedOrganizations />
       <APIssues sectionPath={AC.ISSUES} />
     </div>);
