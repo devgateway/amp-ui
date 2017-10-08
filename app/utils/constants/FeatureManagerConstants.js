f74646df665ea91a78a717ac5f3ecaf808951044
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-10-08 12:35:55 -0300

    AMPOFFLINE-426: Add some checks.

--- app/utils/constants/FeatureManagerConstants.js
+++ app/utils/constants/FeatureManagerConstants.js
@@ -5,11 +5,13 @@
 export const PUBLIC_VIEW_CHANGE_PASSWORD = '/PUBLIC VIEW/Login - User Management/Change Password';
 export const PUBLIC_VIEW_TROUBLE_SIGN_IN = '/PUBLIC VIEW/Login - User Management/Trouble signing in';
 export const ACTIVITY_DELIVERY_RATE = '/PROJECT MANAGEMENT/Funding/Funding Information/Delivery rate';
+export const ACTIVITY_PROJECT_ID_AND_PLANNING = '/PROJECT MANAGEMENT/Project ID and Planning';
 export const ACTIVITY_DURATION_OF_PROJECT = '/PROJECT MANAGEMENT/Project ID and Planning/Planning/Duration of Project';
 export const ACTIVITY_TEAM_LEADER = '/PROJECT MANAGEMENT/Project ID and Planning/Identification/Data Team Leader';
 export const ACTIVITY_SAME_AS_PROPOSED_START_DATE =
   '/Activity Form/Planning/Actual Start Date/Same As Proposed Start Date';
 export const ACTIVITY_SAME_AS_PROPOSED_APPROVAL_DATE =
   '/Activity Form/Planning/Actual Approval Date/Same As Proposed Approval Date';
+export const ACTIVITY_IDENTIFICATION = '/Activity Form/Identification';
 export const ACTIVITY_LAST_UPDATED_BY = '/Activity Form/Identification/Activity Last Updated by';
 export const ACTIVITY_LAST_UPDATED_ON = '/Activity Form/Identification/Activity Updated On';
