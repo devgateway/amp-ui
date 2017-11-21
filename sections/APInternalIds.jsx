404f07f693b2b46769f98eb748605e468407dd21
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-11-21 16:30:54 -0300

    AMPOFFLINE-534: Change constant.

--- sections/APInternalIds.jsx
+++ sections/APInternalIds.jsx
@@ -3,7 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import Tablify from '../components/Tablify';
 import Section from './Section';
 import translate from '../../../../utils/translate';
-import { ACTIVITY_INTERNAL_IDS } from '../../../../utils/constants/ActivityConstants';
+import { ACTIVITY_INTERNAL_IDS, ACTIVITY_INTERNAL_IDS_COLS } from '../../../../utils/constants/ActivityConstants';
 import { ACTIVITY_INTERNAL_IDS_INTERNAL_ID_PATH } from '../../../../utils/constants/FieldPathConstants';
 import styles from '../ActivityPreview.css';
 import Logger from '../../../../modules/util/LoggerManager';
@@ -59,7 +59,7 @@ const APInternalIdsSection = (isSeparateSection) => class extends Component {
     if (isSeparateSection === true) {
       // make sure content exists before formatting
       const noData = <tr><td>{translate('No Data')}</td></tr>;
-      const tableContent = content ? Tablify.addRows(content, 1) : noData;
+      const tableContent = content ? Tablify.addRows(content,  ACTIVITY_INTERNAL_IDS_COLS) : noData;
       content = <div><table className={styles.box_table}><tbody>{tableContent}</tbody></table></div>;
     } else if (content || this.props.showIfEmpty) {
       return (
