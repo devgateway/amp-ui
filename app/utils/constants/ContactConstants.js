e96e43cb0084f997e99427e9d3da7da07cf1d2fd
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-05-09 17:13:18 +0300

    AMPOFFLINE-706 update / reject activities based on contacts push result

--- app/utils/constants/ContactConstants.js
+++ app/utils/constants/ContactConstants.js
@@ -2,3 +2,4 @@ export const CLIENT_CHANGE_ID = 'amp-offline-contact-change-id';
 export const CLIENT_CHANGE_ID_PREFIX = 'amp-offline-contact-id';
 export const INTERNAL_ID = 'internal-id';
 export const NAME = 'name';
+export const LAST_NAME = 'last_name';
