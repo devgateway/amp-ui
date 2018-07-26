b0d76060ef23f506e8319b6cacea05c70d010356
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-07-26 19:33:17 -0300

    AMPOFFLINE-1102: Show more results.

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -45,6 +45,8 @@ export const COLLECTION_RESOURCES = 'resources';
 export const COLLECTION_REPOSITORY = 'repository';
 export const COLLECTION_GAZETTEER = 'gazetteer';
 
+export const GAZETTEER_DISTANCE = 2;
+
 export const DB_FILE_PREFIX = 'database';
 export const DB_FILE_EXTENSION = '.db';
 // TODO: Find a better way to store the key.
