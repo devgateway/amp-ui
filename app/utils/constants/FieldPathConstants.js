04723a7f49fdb5a5808e87ec3f9fc9036ce35466
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-08-09 12:32:04 +0300

    AMPOFFLINE-1130 Disaster Response in AP

--- app/utils/constants/FieldPathConstants.js
+++ app/utils/constants/FieldPathConstants.js
@@ -13,6 +13,7 @@ export const FUNDING_DETAILS_PATH = 'fundings~funding_details';
 export const FUNDING_CURRENCY_PATH = 'fundings~funding_details~currency';
 export const ADJUSTMENT_TYPE_PATH = 'fundings~funding_details~adjustment_type';
 export const TRANSACTION_TYPE_PATH = 'fundings~funding_details~transaction_type';
+export const DISASTER_RESPONSE_PATH = `${AC.FUNDINGS}~${AC.FUNDING_DETAILS}~${AC.DISASTER_RESPONSE}`;
 export const ACTIVITY_INTERNAL_IDS_INTERNAL_ID_PATH = 'activity_internal_ids~internal_id';
 export const ACTIVITY_INTERNAL_IDS_ORGANIZATION_PATH = 'activity_internal_ids~organization';
 export const LOCATION_PATH = 'locations~location';
