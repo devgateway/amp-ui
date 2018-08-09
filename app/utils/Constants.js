094627b6425f132af9e0fbb3b0e3048f495764f9
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-08-09 10:43:32 +0300

    AMPOFFLINE-777 settings change trigger; new urls setting processing

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -22,6 +22,7 @@ export const ACTIVITY_PREVIEW_URL = '/activity/preview';
 export const ACTIVITY_EDIT_URL = '/activity/edit';
 export const UPDATE_URL = '/update';
 export const SETUP_URL = '/setup';
+export const SETTINGS_URL = '/settings';
 export const SYNCUP_REDIRECT_URL = '/syncUp/sync';
 export const SYNCUP_HISTORY_TARGET = 'history';
 export const SYNCUP_SUMMARY_URL = '/syncUpSummary';
