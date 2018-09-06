03ec041adfe13980c42c5befe8a16bb7f74609fc
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-09-06 16:43:14 -0300

    AMPOFFLINE-1167: Add FM entries for funding flows.

--- app/utils/constants/FeatureManagerConstants.js
+++ app/utils/constants/FeatureManagerConstants.js
@@ -15,8 +15,9 @@ export const ACTIVITY_SAME_AS_PROPOSED_APPROVAL_DATE =
 export const ACTIVITY_IDENTIFICATION = '/Activity Form/Identification';
 export const ACTIVITY_LAST_UPDATED_BY = '/Activity Form/Identification/Activity Last Updated by';
 export const ACTIVITY_LAST_UPDATED_ON = '/Activity Form/Identification/Activity Updated On';
+const AF_FUNDING_FUNDINGGROUP_FUNDINGITEM = '/Activity Form/Funding/Funding Group/Funding Item';
 export const ACTIVITY_DISBURSEMENTS_DISBURSEMENT_ORDER_ID =
-  '/Activity Form/Funding/Funding Group/Funding Item/Disbursements/Disbursements Table/Disbursement Order Id';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Disbursement Order Id`;
 export const ACTIVITY_SECTORS = '/Activity Form/Sectors';
 export const ACTIVITY_PRIMARY_SECTORS = '/Activity Form/Sectors/Primary Sectors';
 export const ACTIVITY_SECONDARY_SECTORS = '/Activity Form/Sectors/Secondary Sectors';
@@ -25,17 +26,17 @@ export const ACTIVITY_ORGANIZATIONS_DONOR_ORGANIZATION = '/Activity Form/Organiz
 export const ACTIVITY_PROGRAM = '/Activity Form/Program';
 export const ACTIVITY_PLANNING = '/Activity Form/Planning';
 export const ACTIVITY_COMMITMENTS_PLEDGES =
-  '/Activity Form/Funding/Funding Group/Funding Item/Commitments/Commitments Table/Pledges';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Commitments/Commitments Table/Pledges`;
 export const ACTIVITY_DISBURSEMENTS_PLEDGES =
-  '/Activity Form/Funding/Funding Group/Funding Item/Disbursements/Disbursements Table/Pledges';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Pledges`;
 export const ACTIVITY_EXPENDITURES_PLEDGES =
-  '/Activity Form/Funding/Funding Group/Funding Item/Expenditures/Expenditures Table/Pledges';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Expenditures/Expenditures Table/Pledges`;
 export const ACTIVITY_COMMITMENTS_DISASTER_RESPONSE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Commitments/Commitments Table/Disaster Response';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Commitments/Commitments Table/Disaster Response`;
 export const ACTIVITY_DISBURSEMENTS_DISASTER_RESPONSE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Disbursements/Disbursements Table/Disaster Response';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Disaster Response`;
 export const ACTIVITY_EXPENDITURES_DISASTER_RESPONSE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Expenditures/Expenditures Table/Disaster Response';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Expenditures/Expenditures Table/Disaster Response`;
 export const ACTIVITY_CONTACT = '/Activity Form/Contacts';
 export const ACTIVITY_DOCUMENTS_ADD_DOCUMENT = '/Activity Form/Related Documents/Add New Document';
 export const ACTIVITY_DOCUMENTS_ADD_WEBLINK = '/Activity Form/Related Documents/Add New Web Link';
@@ -45,9 +46,35 @@ export const ACTIVITY_ISSUES_ADD_MEASURE = '/Activity Form/Issues Section/Issue/
 export const ACTIVITY_ISSUES_DELETE_MEASURE = '/Activity Form/Issues Section/Issue/Measure/Delete Item';
 export const ACTIVITY_ISSUES_ADD_ACTOR = '/Activity Form/Issues Section/Issue/Measure/Add Item';
 export const ACTIVITY_ISSUES_DELETE_ACTOR = '/Activity Form/Issues Section/Issue/Measure/Actor/Delete Item';
+
+export const ACTIVITY_COMMITMENTS_FUNDING_FLOWS_ORGROLE_RECIPIENT_ORGROLE =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Commitments/Commitments Table/Funding Flows OrgRole Selector/Recipient Org Role`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_RECIPIENT_ORGROLE =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Recipient Org Role`;
+export const ACTIVITY_COMMITMENTS_FUNDING_FLOWS_ORGROLE_RECIPIENT_ORGANIZATION =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Commitments/Commitments Table/Funding Flows OrgRole Selector/Recipient Organization`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_RECIPIENT_ORGANIZATION =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Recipient Organization`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_IMPLEMENTING_AGENCY =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Implementing Agency`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_CONTRACTING_AGENCY =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Contracting Agency`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_BENEFICIARY_AGENCY =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Beneficiary Agency`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_DONOR_ORGANIZATION =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Donor Organization`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_EXECUTING_AGENCY =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Executing Agency`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_REGIONAL_GROUP =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Regional Group`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_RESPONSIBLE_ORGANIZATION =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Responsible Organization`;
+export const ACTIVITY_DISBURSEMENTS_FUNDING_FLOWS_ORGROLE_ADD_SECTOR_GROUP =
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Funding Flows OrgRole Selector/Add Sector Group`;
+
 export const ACTIVITY_COMMITMENTS_FIXED_EXCHANGE_RATE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Commitments/Commitments Table/Fixed exchange rate';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Commitments/Commitments Table/Fixed exchange rate`;
 export const ACTIVITY_DISBURSEMENTS_FIXED_EXCHANGE_RATE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Disbursements/Disbursements Table/Fixed exchange rate';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Disbursements/Disbursements Table/Fixed exchange rate`;
 export const ACTIVITY_EXPENDITURES_FIXED_EXCHANGE_RATE =
-  '/Activity Form/Funding/Funding Group/Funding Item/Expenditures/Expenditures Table/Fixed exchange rate';
+  `${AF_FUNDING_FUNDINGGROUP_FUNDINGITEM}/Expenditures/Expenditures Table/Fixed exchange rate`;
