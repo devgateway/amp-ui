75d119e0ff5af1616bc25f2ca3ffd451c70700fd
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-09-12 17:01:52 -0300

    AMPOFFLINE-1168: Add conditions field.

--- app/utils/constants/ActivityConstants.js
+++ app/utils/constants/ActivityConstants.js
@@ -47,6 +47,7 @@ export const MODE_OF_PAYMENT = 'mode_of_payment';
 export const FUNDING_CLASSIFICATION_DATE = 'funding_classification_date';
 export const AGREEMENT = 'agreement';
 export const DONOR_OBJECTIVE = 'donor_objective';
+export const CONDITIONS = 'conditions';
 export const AGREEMENT_CODE = 'code';
 export const AGREEMENT_TITLE = 'title';
 export const PROJECT_CATEGORY = 'project_category';
