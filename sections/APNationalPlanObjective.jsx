31b59b3e8516b1946cf390d42d5441fbb3104f8d
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-09-19 14:20:06 +0300

    AMPOFFLINE-1193 move National Plan Objective under Program section in AP

--- sections/APNationalPlanObjective.jsx
+++
@@ -1,8 +0,0 @@
-import Section from './Section';
-import APPercentageList from '../components/APPercentageList';
-import { NATIONAL_PLAN_OBJECTIVE, PROGRAM, PROGRAM_PERCENTAGE } from '../../../../utils/constants/ActivityConstants';
-
-const APNationalPlanObjective = Section(APPercentageList(NATIONAL_PLAN_OBJECTIVE, PROGRAM, PROGRAM_PERCENTAGE),
-'National Plan', true, 'APNationalPlan');
-
-export default APNationalPlanObjective;
--- sections/APProgram.jsx
+++ sections/APProgram.jsx
@@ -1,16 +1,20 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Section from './Section';
 import APPercentageList from '../components/APPercentageList';
 import {
   PRIMARY_PROGRAMS,
   SECONDARY_PROGRAMS,
   PROGRAM,
-  PROGRAM_PERCENTAGE
+  PROGRAM_PERCENTAGE,
+  NATIONAL_PLAN_OBJECTIVE
 } from '../../../../utils/constants/ActivityConstants';
-import styles from '../ActivityPreview.css';
+import * as styles from '../ActivityPreview.css';
 import FieldsManager from '../../../../modules/field/FieldsManager';
 import Logger from '../../../../modules/util/LoggerManager';
 
+const APNationalPlanList = APPercentageList(NATIONAL_PLAN_OBJECTIVE, PROGRAM, PROGRAM_PERCENTAGE,
+  'National Plan Objective');
 const PrimaryProgramList = APPercentageList(PRIMARY_PROGRAMS, PROGRAM, PROGRAM_PERCENTAGE, 'Primary Program');
 const SecondaryProgramList = APPercentageList(SECONDARY_PROGRAMS, PROGRAM, PROGRAM_PERCENTAGE, 'Secondary Program');
 
@@ -30,11 +34,14 @@ class APProgram extends Component {
 
   constructor(props) {
     super(props);
-    logger.log('constructor');
+    logger.debug('constructor');
   }
 
   render() {
     return (<div>
+      <div className={styles.primary_sector}>
+        <APNationalPlanList key="national-plan-list" {...this.props} />
+      </div>
       <div className={styles.primary_sector}>
         <PrimaryProgramList key="primary-programs-list" {...this.props} />
       </div>
