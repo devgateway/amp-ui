64194ff4f2cba1482f0db576711cf7bf6a91ca1d
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-07 16:13:40 +0200

    AMPOFFLINE-1322 Financial Instrument in AP

--- app/utils/constants/ActivityConstants.js
+++ app/utils/constants/ActivityConstants.js
@@ -11,6 +11,7 @@ export const ORG_ROLE_FIELDS = toFieldNames(ValueConstants.ORG_ROLE_NAMES);
 export const ACTIVITY_BUDGET = 'activity_budget';
 export const ACTIVITY_STATUS = 'activity_status';
 export const STATUS_REASON = 'status_reason';
+export const FINANCIAL_INSTRUMENT = 'financial_instrument';
 export const TYPE_OF_IMPLEMENTATION = 'type_of_implementation';
 export const OBJECTIVE = 'objective';
 export const RESULTS = 'results';
