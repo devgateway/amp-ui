cade9f7b3fc1a981c09a1e95bdd0120ad7649bc7
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2019-05-07 19:46:07 +0300

    AMPOFFLINE-531 fix AP order

--- sections/APRelatedOrganizations.jsx
+++ sections/APRelatedOrganizations.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unused-prop-types,class-methods-use-this */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Section from './Section';
 import {
   ADDITIONAL_INFO,
@@ -20,8 +21,8 @@ import { ACTIVITY_ORGANIZATIONS_DONOR_ORGANIZATION } from '../../../../utils/con
 const DO = APPercentageList(DONOR_ORGANIZATION, ORGANIZATION, PERCENTAGE, 'Donor Organization');
 const RO = APPercentageList(RESPONSIBLE_ORGANIZATION, ORGANIZATION, PERCENTAGE, 'Responsible Organization');
 const CA = APPercentageList(CONTRACTING_AGENCY, ORGANIZATION, PERCENTAGE, 'Contracting Agency');
-const BE = APPercentageList(BENEFICIARY_AGENCY, ORGANIZATION, PERCENTAGE, 'Beneficiary Agency');
-const IE = APPercentageList(IMPLEMENTING_AGENCY, ORGANIZATION, PERCENTAGE, 'Implementing Agency');
+const BA = APPercentageList(BENEFICIARY_AGENCY, ORGANIZATION, PERCENTAGE, 'Beneficiary Agency');
+const IA = APPercentageList(IMPLEMENTING_AGENCY, ORGANIZATION, PERCENTAGE, 'Implementing Agency');
 const EA = APPercentageList(EXECUTING_AGENCY, ORGANIZATION, PERCENTAGE, 'Executing Agency');
 const RG = APPercentageList(REGIONAL_GROUP, ORGANIZATION, PERCENTAGE, 'Regional Group');
 const SG = APPercentageList(SECTOR_GROUP, ORGANIZATION, PERCENTAGE, 'Sector Group');
@@ -50,12 +51,12 @@ class APRelatedOrganizations extends Component {
     return (<div>
       <DO key="do-org-list" {...porps} fmPath={ACTIVITY_ORGANIZATIONS_DONOR_ORGANIZATION} />
       <RO key="ro-org-list" {...porps} />
-      <CA key="ca-org-list" {...porps} />
-      <BE key="be-org-list" {...porps} />
-      <IE key="ie-org-list" {...porps} />
       <EA key="ea-org-list" {...porps} />
-      <SG key="sg-org-list" {...porps} />
+      <IA key="ie-org-list" {...porps} />
+      <BA key="be-org-list" {...porps} />
+      <CA key="ca-org-list" {...porps} />
       <RG key="rg-org-list" {...porps} />
+      <SG key="sg-org-list" {...porps} />
     </div>);
   }
 }
