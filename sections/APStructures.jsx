c71fbae24b217f3a0d917dddde5bab5af4b82aa1
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2019-05-10 19:10:46 +0300

    AMPOFFLINE-1361 use keys for coordinates and fix html structure

--- sections/APStructures.jsx
+++ sections/APStructures.jsx
@@ -1,9 +1,11 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Section from './Section';
 import Logger from '../../../../modules/util/LoggerManager';
 import * as AC from '../../../../utils/constants/ActivityConstants';
 import translate from '../../../../utils/translate';
 import styles from '../ActivityPreview.css';
+import * as Utils from '../../../../utils/Utils';
 
 const logger = new Logger('AP structures');
 
@@ -36,7 +38,7 @@ class APStructures extends Component {
 
   constructor(props) {
     super(props);
-    logger.log('constructor');
+    logger.debug('constructor');
   }
 
   getCoordinates(structure) {
@@ -64,7 +66,7 @@ class APStructures extends Component {
       const content = [];
       structure[AC.STRUCTURES_COORDINATES].forEach(c => {
         content.push(
-          <tr>
+          <tr key={Utils.stringToUniqueId()}>
             <td>{buildSimpleField(`${[AC.STRUCTURES]}~${AC.STRUCTURES_COORDINATES}~${[AC.STRUCTURES_LATITUDE]}`,
               true, null, true, c, null, { noTitle: true, fieldValueClass: styles.structures_coordinates_value })}</td>
             <td>{buildSimpleField(`${[AC.STRUCTURES]}~${AC.STRUCTURES_COORDINATES}~${[AC.STRUCTURES_LONGITUDE]}`,
@@ -73,10 +75,12 @@ class APStructures extends Component {
       });
       return (
         <table className={styles.structures_coordinates_table}>
-          <tbody>
-            <thead>
+          <thead>
+            <tr>
               <th><span className={styles.section_field_name}>{translate('Coordinates')}</span></th>
-            </thead>
+            </tr>
+          </thead>
+          <tbody>
             {content}
           </tbody>
         </table>);
