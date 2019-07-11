e601566656b567930bb1cd218184452611605872
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2019-07-11 19:18:17 +0300

    AMPOFFLINE-1458 update

--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -52,8 +52,6 @@ export const GAZETTEER_DISTANCE_DIVIDE = 3;
 
 export const DB_FILE_PREFIX = 'database';
 export const DB_FILE_EXTENSION = '.db';
-// TODO: Find a better way to store the key.
-export const AKEY = 'key';
 export const DB_COMMON_DATASTORE_OPTIONS = { autoload: false, corruptAlertThreshold: 0 };
 export const DB_AUTOCOMPACT_INTERVAL_MILISECONDS = 60000;
 export const DB_DEFAULT_QUERY_LIMIT = 999999999;
