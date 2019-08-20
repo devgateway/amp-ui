c461bb8dc5dfe4df022149ca3ec1e20bcb5bde7b
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2019-08-20 17:22:43 -0300

    AMPUI-42 Migrate ContactConstants and contacts section

--- src/modules/util/ContactConstants.js
+++ src/modules/util/ContactConstants.js
@@ -1,29 +1,58 @@
-import { ValueConstants } from 'amp-ui';
+import ValueConstants from '../../utils/ValueConstants';
 
-export const CLIENT_CHANGE_ID = 'amp-offline-contact-change-id';
-export const CLIENT_CHANGE_ID_PREFIX = 'amp-offline-contact-id';
-export const NAME = 'name';
-export const LAST_NAME = 'last_name';
-export const TITLE = 'title';
-export const ORGANIZATION_NAME = 'organization_name';
-export const FUNCTION = 'function';
-export const OFFICE_ADDRESS = 'office_address';
-export const CREATOR = 'creator';
-export const ORGANISATION_CONTACTS = 'organisation_contacts';
-export const ORGANISATION = 'organisation';
-export const EMAIL = 'email';
-export const VALUE = 'value';
-export const PHONE = 'phone';
-export const EXTENSION_VALUE = 'extension_value';
-export const TYPE = 'type';
-export const FAX = 'fax';
+const CLIENT_CHANGE_ID = 'amp-offline-contact-change-id';
+const CLIENT_CHANGE_ID_PREFIX = 'amp-offline-contact-id';
+const NAME = 'name';
+const LAST_NAME = 'last_name';
+const TITLE = 'title';
+const ORGANIZATION_NAME = 'organization_name';
+const FUNCTION = 'function';
+const OFFICE_ADDRESS = 'office_address';
+const CREATOR = 'creator';
+const ORGANISATION_CONTACTS = 'organisation_contacts';
+const ORGANISATION = 'organisation';
+const EMAIL = 'email';
+const VALUE = 'value';
+const PHONE = 'phone';
+const EXTENSION_VALUE = 'extension_value';
+const TYPE = 'type';
+const FAX = 'fax';
 
-export const TMP_FORM_ID = 'form-id';
-export const TMP_UNIQUE_ID = 'uniqueId';
-export const TMP_HYDRATED = 'hydrated';
-export const TMP_ERRORS = 'errors';
-export const TMP_CONTACT_LOADED = 'isContactLoaded';
-export const TMP_CONTACT_LOADING = 'isContactLoading';
-export const TMP_ENTITY_VALIDATOR = ValueConstants.VC_TMP_ENTITY_VALIDATOR;
-export const TMP_FIELDS = [TMP_FORM_ID, TMP_UNIQUE_ID, TMP_HYDRATED, TMP_ERRORS, TMP_ENTITY_VALIDATOR,
+const TMP_FORM_ID = 'form-id';
+const TMP_UNIQUE_ID = 'uniqueId';
+const TMP_HYDRATED = 'hydrated';
+const TMP_ERRORS = 'errors';
+const TMP_CONTACT_LOADED = 'isContactLoaded';
+const TMP_CONTACT_LOADING = 'isContactLoading';
+const TMP_ENTITY_VALIDATOR = ValueConstants.TMP_ENTITY_VALIDATOR;
+const TMP_FIELDS = [TMP_FORM_ID, TMP_UNIQUE_ID, TMP_HYDRATED, TMP_ERRORS, TMP_ENTITY_VALIDATOR,
   TMP_CONTACT_LOADED, TMP_CONTACT_LOADING];
+
+
+export default Object.freeze({
+  CLIENT_CHANGE_ID,
+  CLIENT_CHANGE_ID_PREFIX,
+  NAME,
+  LAST_NAME,
+  TITLE,
+  ORGANIZATION_NAME,
+  FUNCTION,
+  OFFICE_ADDRESS,
+  CREATOR,
+  ORGANISATION_CONTACTS,
+  ORGANISATION,
+  EMAIL,
+  VALUE,
+  PHONE,
+  EXTENSION_VALUE,
+  TYPE,
+  FAX,
+  TMP_FORM_ID,
+  TMP_UNIQUE_ID,
+  TMP_HYDRATED,
+  TMP_ERRORS,
+  TMP_CONTACT_LOADED,
+  TMP_CONTACT_LOADING,
+  TMP_ENTITY_VALIDATOR,
+  TMP_FIELDS
+});
