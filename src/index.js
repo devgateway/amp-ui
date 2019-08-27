012d52aa7fdf4e41f4716a1d260161c219a53be0
Gabriel Inchauspe <ginchauspe@gmail.com>
2019-08-27 10:51:01 -0300

    AMPUI-60: Remove exports.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ import CurrencyRatesManager from './modules/util/CurrencyRatesManager';
 import PossibleValuesManager from './modules/field/PossibleValuesManager';
 import FeatureManager from './modules/util/FeatureManager';
 import APLabel from './activity/preview/components/APLabel.jsx';
-import APField from './activity/preview/components/APField.jsx';
-import Tablify from './activity/preview/components/Tablify.jsx';
-import APPercentageField from './activity/preview/components/APPercentageField.jsx';
-import APPercentageList from './activity/preview/components/APPercentageList.jsx';
-import Section from './activity/preview/sections/Section.jsx';
 import FundingSummary from './activity/preview/sections/FundingSummary.jsx';
 import APIdentification from './activity/preview/sections/APIdentification.jsx';
 import UIUtils from './utils/UIUtils';
@@ -22,7 +17,6 @@ import APPlanning from './activity/preview/sections/APPlanning.jsx';
 import AdditionalInfo from './activity/preview/sections/AdditionalInfo.jsx';
 import APInternalIds from './activity/preview/sections/APInternalIds.jsx';
 import APComponents from './activity/preview/sections/APComponents.jsx';
-import APProgram from './activity/preview/sections/APProgram.jsx';
 import APDocument from './activity/preview/sections/APDocument.jsx';
 import APContact from './activity/preview/sections/APContact.jsx';
 import UserConstants from './utils/constants/UserConstants';
@@ -34,8 +28,6 @@ import Loading from './activity/common/Loading.jsx';
 import ResourceConstants from './utils/constants/ResourceConstants';
 import CalendarConstants from './modules/util/CalendarConstants';
 import APFundingSources from './activity/preview/sections/APFundingSources.jsx';
-import APLocation from './activity/preview/sections/APLocation.jsx';
-import APSector from './activity/preview/sections/APSector.jsx';
 import APRelatedOrganizations from './activity/preview/sections/APRelatedOrganizations.jsx';
 import { APProposedProjectCost, APRevisedProjectCost } from './activity/preview/sections/APProjectCost.jsx';
 import APFundingTotalItem from './activity/preview/sections/funding/APFundingTotalItem.jsx';
@@ -46,12 +38,6 @@ import APFundingItem from './activity/preview/sections/funding/APFundingItem.jsx
 import APFundingTransactionTypeItem from './activity/preview/sections/funding/APFundingTransactionTypeItem.jsx';
 import APFundingOrganizationSection from './activity/preview/sections/funding/APFundingOrganizationSection.jsx';
 import APFundingSection from './activity/preview/sections/funding/APFundingSection.jsx';
-// TODO to remove the components that are not going to be used directly in OFFLINE or in ONLINE and are
-// TODO Internal components
-import APStructures from './activity/preview/sections/APStructures.jsx';
-import APActor from './activity/preview/sections/issues/APActor.jsx';
-import APMeasure from './activity/preview/sections/issues/APMeasure.jsx';
-import APIssues from './activity/preview/sections/issues/APIssues.jsx';
 import downloadImage from './assets/images/download.svg';
 import ajaxLoader from './assets/images/ajax-loader.gif';
 import gotoImage from './assets/images/goto_url.svg';
@@ -62,7 +48,6 @@ import ActivityPreviewUI from './activity/preview/ActivityPreviewUI.jsx';
 
 export {
   Constants,
-  APProgram,
   ActivityConstants,
   FeatureManagerConstants,
   ErrorConstants,
@@ -74,12 +59,7 @@ export {
   PossibleValuesManager,
   FeatureManager,
   APLabel,
-  APField,
-  Tablify,
-  APPercentageField,
-  APPercentageList,
   UIUtils,
-  Section,
   FundingSummary,
   AdditionalInfo,
   UserConstants,
@@ -97,8 +77,6 @@ export {
   ContactConstants,
   APContact,
   APFundingSources,
-  APLocation,
-  APSector,
   APRelatedOrganizations,
   APProposedProjectCost,
   APRevisedProjectCost,
@@ -110,10 +88,6 @@ export {
   APFundingTransactionTypeItem,
   APFundingOrganizationSection,
   APFundingSection,
-  APStructures,
-  APActor,
-  APMeasure,
-  APIssues,
   downloadImage,
   ajaxLoader,
   gotoImage,
@@ -125,7 +99,6 @@ export {
 
 export default {
   Constants,
-  APProgram,
   ActivityConstants,
   FeatureManagerConstants,
   ErrorConstants,
@@ -137,12 +110,7 @@ export default {
   PossibleValuesManager,
   FeatureManager,
   APLabel,
-  APField,
-  Tablify,
-  APPercentageField,
-  APPercentageList,
   UIUtils,
-  Section,
   FundingSummary,
   AdditionalInfo,
   UserConstants,
@@ -160,8 +128,6 @@ export default {
   ContactConstants,
   APContact,
   APFundingSources,
-  APLocation,
-  APSector,
   APRelatedOrganizations,
   APProposedProjectCost,
   APRevisedProjectCost,
@@ -173,10 +139,6 @@ export default {
   APFundingTransactionTypeItem,
   APFundingOrganizationSection,
   APFundingSection,
-  APStructures,
-  APActor,
-  APMeasure,
-  APIssues,
   downloadImage,
   ajaxLoader,
   gotoImage,
