1ca9cf865581ecd7c46db220f382168f67c8fa42
Gabriel Inchauspe <ginchauspe@gmail.com>
2019-10-02 15:01:21 -0300

    AMPUI-86: Adding summary section.

--- src/utils/export/GenerateWordPreview.js
+++ src/utils/export/GenerateWordPreview.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ActivityConstants from '../../modules/util/ActivityConstants';
+import FieldPathConstants from '../FieldPathConstants';
+import FeatureManager from '../../modules/util/FeatureManager';
+import PossibleValuesManager from '../../modules/field/PossibleValuesManager';
 
 const FileSaver = require('file-saver');
 const docx = require('docx');
@@ -11,7 +14,10 @@ let _context;
 let _rtl;
 let styles;
 
-// TODO: check if this class can be a Component too.
+/**
+ * Useful resources: https://github.com/dolanmiu/docx/wiki/Styling-with-JS,
+ * https://runkit.com/dolanmiu/docx-demo2
+ */
 export default class GenerateWordPreview {
   static _gwp = new GenerateWordPreview();
 
@@ -27,6 +33,7 @@ export default class GenerateWordPreview {
     this.createDocument();
     this.createStyles();
     this.addContentTitleSection();
+    this.addSummarySection();
     this.download();
   }
 
@@ -39,19 +46,32 @@ export default class GenerateWordPreview {
   }
 
   static addContentTitleSection() {
-    const title = document.createParagraph();
-    title.createTextRun(_props.activity[ActivityConstants.PROJECT_TITLE].toString());
-    // title.style('ProjectTitle');
-    title.heading1();
-    if (_rtl) {
-      // dont enable or it will also change the writing function.
-      // title.bidirectional();
-      title.right();
+    this.createSimpleParagraphElement(_props.activity[ActivityConstants.PROJECT_TITLE], 'ProjectTitle');
+  }
+
+  static addSummarySection() {
+    const items = [ActivityConstants.AMP_ID, ActivityConstants.ACTIVITY_STATUS, ActivityConstants.ACTIVITY_BUDGET];
+    items.map(i => {
+      this.createSimpleParagraphElement(this.buildSimpleField(i, true));
+    });
+  }
+
+  static createSimpleParagraphElement(text, styleName, options) {
+    const p = document.createParagraph();
+    p.createTextRun(text);
+    if (styleName) {
+      p.style(styleName);
     }
+    if (_rtl) {
+      // Dont enable or it will also change the writing function.
+      // title.bidirectional();
+      p.right();
+    }
+    return p;
   }
 
   static download() {
-    const packer = new Packer(document, styles);
+    const packer = new Packer();
     packer.toBlob(document)
       .then(blob => {
         console.log(blob);
@@ -61,8 +81,7 @@ export default class GenerateWordPreview {
   }
 
   static createStyles() {
-    styles = new docx.Styles();
-    styles.createParagraphStyle('ProjectTitle', 'Project Title')
+    document.Styles.createParagraphStyle('ProjectTitle', 'Project Title')
       .basedOn('Normal')
       .next('Normal')
       .quickFormat()
@@ -72,6 +91,57 @@ export default class GenerateWordPreview {
       .color('black')
       .spacing({ after: 120 });
 
-    styles.createParagraphStyle('RTL', 'RTL').right();
+    document.Styles.createParagraphStyle('RTL', 'RTL').right();
+  }
+
+  static buildSimpleField(path, showIfNotAvailable, NAOptions: Set, inline = false, parent = null, fieldsManager = null
+    , options) {
+    const options_ = options || {};
+    const fmPath = FieldPathConstants.ACTIVITY_FIELDS_FM_PATH[path];
+    fieldsManager = fieldsManager || _context.activityFieldsManager;
+    if (fieldsManager.isFieldPathEnabled(path)
+      && (!fmPath || FeatureManager.isFMSettingEnabled(fmPath, false))) {
+      const title = (options_.noTitle ? '' : fieldsManager.getFieldLabelTranslation(path));
+      let valuePath = path;
+      if (parent) {
+        const fieldPathParts = path.split('~');
+        valuePath = fieldPathParts[fieldPathParts.length - 1];
+      }
+      const alternatePath = FieldPathConstants.ALTERNATE_VALUE_PATH[valuePath];
+      let value = fieldsManager.getValue(parent || _props.activity, valuePath,
+        PossibleValuesManager.getOptionTranslation);
+      if ((value === null || value === undefined) && alternatePath) {
+        value = fieldsManager.getValue(_props.activity, alternatePath,
+          PossibleValuesManager.getOptionTranslation);
+      }
+      const fieldDef = fieldsManager.getFieldDef(path);
+      if (fieldDef.field_type === FieldPathConstants.FIELD_TYPE_DATE) {
+        value = _context.DateUtils.createFormattedDate(value);
+      } else if (fieldDef.field_type === FieldPathConstants.FIELD_TYPE_TIMESTAMP) {
+        // matching AP online to format as date for now
+        value = _context.DateUtils.createFormattedDate(value);
+      } else if (Array.isArray(value) && !value.length) {
+        value = null;
+      }
+      value = NAOptions && NAOptions.has(value) ? null : value;
+
+      const re = /^\s?$/; // check for whitespace
+      if (re.test(value)) value = value.trim(); // check for whitespace
+      if (value === '' || value === null) {
+        value = _context.translate('No Data');
+      }
+      if (showIfNotAvailable === true || (value !== undefined && value !== null)) {
+        const useInnerHTML = FieldPathConstants.RICH_TEXT_FIELDS.has(path);
+        /* return (<APField
+          key={UIUtils.stringToUniqueId(path)} title={title} value={value} useInnerHTML={useInnerHTML}
+          inline={inline}
+          separator={false}
+          fieldClass={options_.fieldClass || _props.fieldClass}
+          fieldNameClass={_props.fieldNameClass}
+          fieldValueClass={options_.fieldValueClass || _props.fieldValueClass}
+          translate={_context.translate} />);*/
+        return `${title}: ${_context.translate(value)}`;
+      }
+    }
   }
 }
