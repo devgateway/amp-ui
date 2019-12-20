d07353bcbae6cb4945f0d5a3f2be60f6055c59ba
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2019-12-20 09:51:55 -0300

    AMPUI-90 reorder funding items

--- src/utils/helpers/CommonActivityHelper.js
+++ src/utils/helpers/CommonActivityHelper.js
@@ -15,6 +15,33 @@ const CommonActivityHelper = {
       status = Constants.ACTIVITY_STATUS_UNVALIDATED;
     }
     return status;
+  },
+  sortFundingItems(items, reorderFundingItemId) {
+    let reorderFunction;
+    switch (reorderFundingItemId) {
+      case Constants.ORDER_FUNDING_ITEM_TRANSACTION_DATE_DESC:
+        reorderFunction = (a, b) => Date.parse(b[ActivityConstants.TRANSACTION_DATE])
+          - Date.parse(a[ActivityConstants.TRANSACTION_DATE]);
+        break;
+      case Constants.ORDER_FUNDING_ITEM_TRANSACTION_DATE_ASC:
+        reorderFunction = (a, b) => Date.parse(a[ActivityConstants.TRANSACTION_DATE])
+          - Date.parse(b[ActivityConstants.TRANSACTION_DATE]);
+        break;
+      case Constants.ORDER_FUNDING_ITEM_INSERTION_ORDER_DESC:
+        reorderFunction = (a, b) => Date.parse(b[ActivityConstants.REPORTING_DATE])
+          - Date.parse(b[ActivityConstants.REPORTING_DATE]);
+        break;
+      case Constants.ORDER_FUNDING_ITEM_INSERTION_ORDER_ASC:
+        reorderFunction = (a, b) => Date.parse(a[ActivityConstants.REPORTING_DATE])
+          - Date.parse(b[ActivityConstants.REPORTING_DATE]);
+        break;
+      default:
+        reorderFunction = (a, b) => Date.parse(b[ActivityConstants.TRANSACTION_DATE])
+          - Date.parse(a[ActivityConstants.TRANSACTION_DATE]);
+        break;
+    }
+    items.sort(reorderFunction);
   }
+
 };
 export default CommonActivityHelper;
