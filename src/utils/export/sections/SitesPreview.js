0b14c8bb3290a96bf55c8c2875bc41b06da1a0bc
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2020-02-07 16:40:05 -0300

    AMPUI-137 add missing structure coordinates

--- src/utils/export/sections/SitesPreview.js
+++ src/utils/export/sections/SitesPreview.js
@@ -6,6 +6,7 @@ export default class SitesPreview extends PreviewSection {
   generateSection() {
     super.generateSection();
     if (this.checkIfEnabled()) {
+      const { translate, activityFieldsManager } = this._context;
       this.createSimpleLabel(this._context.translate('Structures'), ExportConstants.STYLE_HEADING2);
       if (this._props.activity[ActivityConstants.STRUCTURES]) {
         this._props.activity[ActivityConstants.STRUCTURES].sort((a, b) =>
@@ -28,6 +29,22 @@ export default class SitesPreview extends PreviewSection {
               });
             this.createField(title.title, title.value);
             this.createField(desc.title, desc.value);
+            const COORDINATES_PATH = `${[ActivityConstants.STRUCTURES]}~${[ActivityConstants.STRUCTURES_TITLE]}`;
+            const LATITUDE_PATH = `${[ActivityConstants.STRUCTURES]}~${[ActivityConstants.STRUCTURES_LATITUDE]}`;
+            const LONGITUDE_PATH = `${[ActivityConstants.STRUCTURES]}~${[ActivityConstants.STRUCTURES_LONGITUDE]}`;
+            if (activityFieldsManager.isFieldPathEnabled(COORDINATES_PATH)
+              && s[ActivityConstants.STRUCTURES_COORDINATES]
+              && s[ActivityConstants.STRUCTURES_COORDINATES].length > 0) {
+              this.createField(translate('Coordinates'),
+                s[ActivityConstants.STRUCTURES_COORDINATES].map(ss =>
+                  `${ss[ActivityConstants.STRUCTURES_LATITUDE]} ${ss[ActivityConstants.STRUCTURES_LONGITUDE]}`)
+                  .join('|'));
+            } else if (activityFieldsManager.isFieldPathEnabled(LATITUDE_PATH)
+              && activityFieldsManager.isFieldPathEnabled(LONGITUDE_PATH)
+              && s[ActivityConstants.STRUCTURES_LATITUDE] && s[ActivityConstants.STRUCTURES_LONGITUDE]) {
+              this.createField('Latitude', s[ActivityConstants.STRUCTURES_LATITUDE]);
+              this.createField('Longitude', s[ActivityConstants.STRUCTURES_LONGITUDE]);
+            }
           });
       }
       this.createSeparator();
