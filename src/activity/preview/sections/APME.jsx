9d208c948def87eedb9b49b184e112853c28256d
Gabriel Inchauspe <ginchauspe@gmail.com>
2020-05-20 21:03:26 -0300

    AMPOFFLINE-1516: Fix problem m&e constants.

--- src/activity/preview/sections/APME.jsx
+++ src/activity/preview/sections/APME.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Section from './Section.jsx';
 import ActivityConstants from '../../../modules/util/ActivityConstants';
 import styles from '../ActivityPreview.css';
 import APField from '../components/APField.jsx';
+import FeatureManager from '../../../modules/util/FeatureManager';
+import FeatureManagerConstants from '../../../modules/util/FeatureManagerConstants';
 
 let logger = null;
 
@@ -37,24 +40,27 @@ class APME extends Component {
         <tbody>
           <tr key={Math.random()}>
             <td>
-              <APField
-                key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_VALUE}: `)}
-                value={indicator[s][ActivityConstants.INDICATOR_VALUE]} inline={false} separator={false}
-                fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} />
+              {FeatureManager.isFMSettingEnabled(FeatureManagerConstants[`ME_ITEM_${s.toUpperCase()}_VALUE_BASE_VALUE`])
+                ? <APField
+                  key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_VALUE}: `)}
+                  value={indicator[s][ActivityConstants.INDICATOR_VALUE]} inline={false} separator={false}
+                  fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} /> : null}
             </td>
             <td>
-              <APField
-                key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_DATE}: `)}
-                value={indicator[s][ActivityConstants.INDICATOR_DATE]} inline={false} separator={false}
-                fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} />
+              {FeatureManager.isFMSettingEnabled(FeatureManagerConstants[`ME_ITEM_${s.toUpperCase()}_VALUE_BASE_DATE`]) ?
+                <APField
+                  key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_DATE}: `)}
+                  value={indicator[s][ActivityConstants.INDICATOR_DATE]} inline={false} separator={false}
+                  fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} /> : null}
             </td>
           </tr>
           <tr key={Math.random()}>
             <td colSpan={2}>
-              <APField
-                key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_COMMENT}: `)}
-                value={indicator[s][ActivityConstants.INDICATOR_COMMENT]} inline={false} separator={false}
-                fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} />
+              {FeatureManager.isFMSettingEnabled(FeatureManagerConstants[`ME_ITEM_${s.toUpperCase()}_VALUE_BASE_COMMENTS`]) ?
+                <APField
+                  key={Math.random()} title={translate(`${s} ${ActivityConstants.INDICATOR_COMMENT}: `)}
+                  value={indicator[s][ActivityConstants.INDICATOR_COMMENT]} inline={false} separator={false}
+                  fieldNameClass={styles.box_field_name} fieldValueClass={styles.box_field_value} /> : null}
             </td>
           </tr>
         </tbody>
