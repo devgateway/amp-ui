02d1ef15cb525d7eca611aaf0bb0b928b4a54d2a
ginchauspe <ginchauspe@gmail.com>
2020-10-31 23:27:14 -0300

    AMP-29811: Show indirect programs.

--- src/activity/preview/components/APPercentageField.jsx
+++ src/activity/preview/components/APPercentageField.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import NumberUtils from '../../../utils/NumberUtils';
 import styles from '../ActivityPreview.css';
+import UIUtils from '../../../utils/UIUtils';
 
 let logger = null;
 
@@ -14,6 +15,7 @@ export default class APPercentageField extends Component {
     value: PropTypes.number,
     titleClass: PropTypes.string,
     valueClass: PropTypes.string,
+    subList: PropTypes.object
   };
 
   static contextTypes = {
@@ -25,14 +27,28 @@ export default class APPercentageField extends Component {
     const { Logger } = this.context;
     logger = new Logger('AP percentage field');
     logger.log('constructor');
+    this.generateSubList = this.generateSubList.bind(this);
+  }
+
+  generateSubList() {
+    const { subList } = this.props;
+    const content = [];
+    subList.forEach(i => {
+      content.push(<APPercentageField
+        key={UIUtils.stringToUniqueId(i.title)} title={i.title} value={i.percentage}
+        titleClass={this.props.titleClass} valueClass={this.props.valueClass} />);
+    });
+    return <div>{content}</div>;
   }
 
   render() {
+    const { subList } = this.props;
     const percentage = this.props.value !== undefined && this.props.value !== null
       ? `${NumberUtils.rawNumberToFormattedString(this.props.value, true)}%` : null;
     return (<div>
       <span className={this.props.titleClass}>{this.props.title} </span>
       <span className={`${this.props.valueClass} ${styles.alignRight}`}>{percentage}</span>
+      {subList ? this.generateSubList() : null}
     </div>);
   }
 }
